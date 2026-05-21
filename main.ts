8971dd538651596d43ae1ff3c83a6fdb7e971114
francinegarcia-create <francinegarcia@prof.educacao.sp.gov.br>
2026-05-20 22:29:25 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+})
